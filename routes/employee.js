c9acf21ddea07a79d5db2295009434aba13e7069
gillmourtunhira <gillmourtunhira@gmail.com>
2020-07-21 16:52:14 +0200

    An extensive human resources system for Harare Institute of Technology, part of a practical job assessment (done in 2 days) :

--- routes/employee.js
+++ routes/employee.js
@@ -5,8 +5,6 @@ const Employee = require('./models/employee');
 // Middleware
 const router = express.Router();
 
-const users = ['Melissa', 'Atipaishe', 'Talia', 'Victoria'];
-
 router.get('/', (req, res, next) => {
     res.render('./employee/index', {
         title: 'Employees View',
