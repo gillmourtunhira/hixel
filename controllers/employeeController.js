6c8345d28038d9294f3a9c321b9ac1d55a9fb52e
gillmourtunhira <gillmourtunhira@gmail.com>
2020-07-26 16:41:01 +0200

    create form in views

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -4,3 +4,10 @@ exports.profile = ((req, res, next) => {
         data: 'Employee Details'
     });
 });
+
+exports.create = ((req, res, next) => {
+    res.render('./employee/create', {
+        title: 'Hixel | Create New Profile',
+        data: 'New Employee Profile'
+    });
+});
--- routes/hixel.js
+++ routes/hixel.js
@@ -6,6 +6,9 @@ const homeController = require('./../controllers/homeController');
 const adminController = require('./../controllers/adminController');
 const employeeController = require('./../controllers/employeeController');
 const leaveController = require('./../controllers/leaveController');
+const {
+    route
+} = require('.');
 
 // Home Route
 router.get('/', homeController.home);
@@ -15,6 +18,7 @@ router.get('/admin', adminController.index);
 router.get('/admin/employees', adminController.employees);
 router.get('/admin/dashboard', adminController.dashboard);
 router.get('/admin/leave', leaveController.leave);
+router.get('/admin/employees/create', employeeController.create);
 
 // Employee Routes
 router.get('/employee/:id/profile', employeeController.profile);
